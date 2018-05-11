1c89922b7fbd8469efb444c8dbf2668cc5b800f7
Joseph Anthony Castillon <AnthonyCastillon@Josephs-MacBook-Pro.local>
2018-05-10 20:57:44 -0400

    Add new item textfield and handle successful action

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     
-    let itemArray = ["Pay bills", "Buy groceries", "Go to library"];
+    var itemArray = ["Pay bills", "Buy groceries", "Go to library"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +51,31 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    
+    //MARK: - Add Todo ITems
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addAction(alertAction)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New todo item"
+            print(alertTextField.text!)
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
     
 }
 
